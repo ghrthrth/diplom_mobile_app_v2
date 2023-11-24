200c8fd73ee61c82af6ebc0b0832b786c08c06b9
Dzmitriy Skarakhodau <dima.skorohodov01@mail.ru>
2023-11-25 01:16:49 +0300

    add error message if user dosen't make permission for coordinatns

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.home;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
@@ -15,6 +17,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
@@ -27,6 +31,9 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Objects;
 
 
 public class HomeFragment extends Fragment {
@@ -36,6 +43,7 @@ private FusedLocationProviderClient fusedLocationClient;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private double lat;
     private double lon;
+    Handler handler = new Handler();
 
     private final LocationCallback locationCallback = new LocationCallback() {
         @Override
@@ -60,33 +68,25 @@ private FusedLocationProviderClient fusedLocationClient;
 
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(binding.getRoot().getContext());
-
         // Проверяем разрешение на доступ к местоположению
         if (ActivityCompat.checkSelfPermission(binding.getRoot().getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // Если разрешение не предоставлено, запрашиваем его у пользователя
             ActivityCompat.requestPermissions((Activity) binding.getRoot().getContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         } else {
+            // Если разрешение уже предоставлено, запускаем метод получения местоположения
             getDeviceLocation();
         }
+
         return root;
     }
 
-
     public void getDeviceLocation() {
-
         // Запрашиваем обновления местоположения
         LocationRequest locationRequest = LocationRequest.create();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         locationRequest.setInterval(10000); // Время между обновлениями в миллисекундах
 
         if (ActivityCompat.checkSelfPermission(binding.getRoot().getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(binding.getRoot().getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         fusedLocationClient.requestLocationUpdates(locationRequest,
@@ -94,6 +94,7 @@ private FusedLocationProviderClient fusedLocationClient;
                 null);
     }
 
+    @SuppressLint("ShowToast")
     public void onResume() {
         LocationManager locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
@@ -108,6 +109,20 @@ private FusedLocationProviderClient fusedLocationClient;
             alert.show();
         }
         super.onResume();
+        if (ActivityCompat.checkSelfPermission(binding.getRoot().getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Если разрешение не предоставлено, запрашиваем его у пользователя
+            ActivityCompat.requestPermissions((Activity) binding.getRoot().getContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            Toast.makeText(binding.getRoot().getContext(), "Необходимо предоставить разрешение на доступ к местоположению в настройках приложения", Toast.LENGTH_SHORT).show();
+            // Создаем задержку в 3 секунды
+
+                    // Удаляем сообщение об ошибке
+                    Toast.makeText(binding.getRoot().getContext(), "", Toast.LENGTH_SHORT).cancel();
+                    // Завершаем приложение
+                    requireActivity().finishAffinity();
+        } else {
+            // Если разрешение уже предоставлено, запускаем метод получения местоположения
+            getDeviceLocation();
+        }
     }
 
     @Override
