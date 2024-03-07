c2cd5956db3ae5381442acd6ebee3be52ce95870
ghrthrth <dima.skorohodov01@mail.ru>
2024-03-07 18:54:31 +0300

    change any functionals

--- app/src/main/java/com/example/myapplication/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/myapplication/ui/gallery/GalleryFragment.java
@@ -238,7 +238,7 @@ public class GalleryFragment extends Fragment {
             MediaType JSON = MediaType.parse("application/json; charset=utf-8");
             RequestBody requestBody = RequestBody.create(JSON, json.toString());
             Request request = new Request.Builder()
-                    .url("https://claimbe.store/diplom/index.php")
+                    .url("https://claimbe.store/diplom/api/add.php")
                     .post(requestBody)
                     .build();
 
